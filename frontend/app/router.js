5f08c03c819ae20d2425613432c5b9acf7f01d5f
Vincent Huang <vincent.007.cn@gmail.com>
2015-03-18 20:10:17 +0800

    add dashboard admin controller

--- frontend/app/router.js
+++ frontend/app/router.js
@@ -6,12 +6,14 @@ var Router = Ember.Router.extend({
 });
 
 Router.map(function() {
+
   // under site namespace
   this.route('site/about', { path: '/about' });
 
 
   // under admin namespace
   this.route('admin/dashboard', { path: '/admin' });
+
 });
 
 export default Router;
