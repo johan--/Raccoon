39eaab929e03b256b3d88e1a294ea032e82bbbcb
Vincent Huang <vincent.007.cn@gmail.com>
2015-04-25 08:51:42 +0800

    fix critical error when routing with /admin url

--- adminpanel/app/router.js
+++ adminpanel/app/router.js
@@ -2,7 +2,8 @@ import Ember from 'ember';
 import config from './config/environment';
 
 var Router = Ember.Router.extend({
-  location: config.locationType
+  location: config.locationType,
+  rootURL: '/admin/'
 });
 
 export default Router.map(function() {
--- config/initializers/ember.rb
+++ config/initializers/ember.rb
@@ -1,4 +1,5 @@
 EmberCLI.configure do |c|
-  c.app :frontend, build_timeout: 10
-  c.app :adminpanel, build_timeout: 10
+  c.app :frontend, build_timeout: 15
+  c.app :adminpanel, 
+    build_timeout: 15
 end
